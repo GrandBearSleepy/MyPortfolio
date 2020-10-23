f6deebccf84ce450f787ac48538f2f1515b84965
Leon <hq_li1982@hotmail.com>
2020-10-23 23:42:55 +0800

    add back top button

--- Assets/script/script.js
+++ Assets/script/script.js
@@ -6,7 +6,14 @@ $(document).ready(function () {
         else {
             $(".navbar").removeClass("sticky");
         }
+        if (this.scrollY > 600) {
+            $(".back-btn").show();
+        }
+        else {
+            $(".back-btn").hide();
+        }
     })
+
     //nav bar
     $(".mune-btn").click(function () {
         $(".navbar .menu").toggleClass("active");
